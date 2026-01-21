1cbd805ce5e93de3759a1f3d0c011a576b0cab62
LABORATORIO B <LABORATORIO B@LAB.EDU.BO>
2026-01-21 17:42:08 -0400

    Proyecto C# de prueba con defectos para auditoría estática

--- DemoAuditApp/Program.cs
+++ DemoAuditApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DemoAuditApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su edad:");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad > 18)
+                Console.WriteLine("Acceso permitido");
+            else
+                Console.WriteLine("Acceso denegado");
+        }
+    }
+}
